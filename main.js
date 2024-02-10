8b57484af00134acf98de62f215d319782568fc3
Samuel Gesang <sgesang@mailo.com>
2024-02-10 22:01:04 +0100

    add new asset categories

--- main.js
+++ main.js
@@ -21,12 +21,16 @@ export let userAssets = [
   { id: 1, name: "Ethereum", quantity: 0.029, notes: "", type: "crypto", abb: "eth", value: 10_000, baseValue: 4_000, isDeleted: false },
   { id: 2, name: "Silver", quantity: 754, notes: "", type: "metals", abb: "silver", value: 10_000, baseValue: 20.7, isDeleted: false },
   { id: 3, name: "Gold", quantity: 3.5, notes: "recently bought", type: "metals", abb: "gold", value: 10_000, baseValue: 1_900, isDeleted: false },
+  { id: 4, name: "Euro", quantity: 200, notes: "under the Pillow", type: "cash", abb: "eur", value: 200, baseValue: 1, isDeleted: false },
+  { id: 5, name: "beach house", quantity: 1, notes: "I wish", type: "real_estate", abb: "RE", value: 1, baseValue: 1, isDeleted: false },
 ];
 
 export const assetUnits = {
   stocks: "Piece/s",
   metals: "Oz",
   crypto: "Piece/s",
+  real_estate: "Piece/s",
+  cash: "Piece/s",
 };
 
 //SETUP
@@ -54,13 +58,14 @@ export function calcTotalValue() {
 }
 
 async function getUserAssetsFromLocalStorage() {
+  console.log(userAssets);
   console.log("Local Storage = ", localStorage.getItem("userAssets"));
-  if (localStorage.getItem("userAssets") == null || localStorage.getItem("userAssets") == "undefined") {
+  if (localStorage.getItem("userAssets") === null || localStorage.getItem("userAssets") === "undefined") {
     console.log("could not get userAssets from Local Storage.");
-    return NaN;
+  } else {
+    userAssets = JSON.parse(localStorage.getItem("userAssets"));
+    console.log("got userAssets from Local Storage.", userAssets);
   }
-  userAssets = JSON.parse(localStorage.getItem("userAssets"));
-  console.log("got userAssets from Local Storage.", userAssets);
   updateView();
 }
 
